72f592ffddaa1a0c572d28f2ba096c77e2eb4321
ryan suh <ryansuh2002@gmail.com>
2024-11-22 23:32:29 -0500

    Initial Commit

---
+++ MyNews/MyNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyNewsApp.swift
+//  MyNews
+//
+//  Created by ryan suh on 11/22/24.
+//
+
+import SwiftUI
+
+@main
+struct MyNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
