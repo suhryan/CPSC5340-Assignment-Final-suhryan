55dab29dfc174621263e26b9e74109f3177d0403
ryan suh <ryansuh2002@gmail.com>
2024-11-23 23:25:53 -0500

    -ArticleViewModel: modified to handle articles based on topics or general headliners listing -added NewsFeedView and ArticleDetailView

---
+++ MyNews/Views/ArticleDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  ArticleDetailView.swift
+//  MyNews
+//
+//  Created by ryan suh on 11/23/24.
+//
+
+import SwiftUI
+
+struct ArticleDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ArticleDetailView()
+}
---
+++ MyNews/Views/NewsFeedView.swift
@@ -0,0 +1,18 @@
+//
+//  NewsFeedView.swift
+//  MyNews
+//
+//  Created by ryan suh on 11/23/24.
+//
+
+import SwiftUI
+
+struct NewsFeedView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NewsFeedView()
+}
